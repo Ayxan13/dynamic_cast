59236e456a0ed8507fb6da154d4a88712028539a
Ayxan Haqverdili <aykhanhagverdili@gmail.com>
2021-08-28 18:11:59 +0400

    firebase dependency added

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,20 @@ import 'package:dynamic_cast/gui/screens/sign_in/sign_in_or_up.dart';
 import 'package:dynamic_cast/i18n/translation.dart';
 import 'package:flutter/material.dart';
 
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_performance/firebase_performance.dart';
+// TODO: Performance tests
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+
+late FirebaseAnalytics firebaseAnalytics;
+
 void main() {
+  firebaseAnalytics = FirebaseAnalytics();
+
+  FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
   runApp(TheApp());
 }
 
